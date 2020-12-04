2f74194d4869c2a91ef9de39196fd503783544d2
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-12-04 13:39:24 +0000

    add user Add Movie feature

--- userInterface.js
+++ userInterface.js
@@ -1,3 +1,14 @@
-function UserInterface() {
-    
-}
+function UserInterface() {}
+
+UserInterface.prototype.addMovieToUserInterface = function(newMovie) {
+    const movieList = document.getElementById("movies");
+
+    movieList.innerHTML += `
+        <tr>
+            <td><img src="${newMovie.url}" class="img-fluid img-thumbnail"></td>
+            <td>${newMovie.title}</td>
+            <td>${newMovie.director}</td>
+            <td><a href="#" id = "delete-film" class = "btn btn-danger">Delete Movie</a></td>
+        </tr>
+    `;
+};
