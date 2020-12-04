e0a427544d9d780cecd9d87254f25719ad4288d4
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-12-04 11:51:59 +0000

    add initial project.js content

---
+++ project.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("movie-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
